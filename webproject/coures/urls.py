efa0c0ed86a58e97c281b184da5b0178c33d9d03
kitti <kittiphod.ka.65@ubu.ac.th>
2024-12-03 06:14:40 +0700

    update

--- webproject/coures/urls.py
+++ webproject/coures/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('course_search/', course_search, name='course_search'),
     path('update/<str:pk>/', CourseUpdateView.as_view(), name='update'),
-]
+    path('delete/<int:pk>/', CourseDeleteView.as_view(), name='delete'),
+    path('courses/', course_list, name='course_list'),
+
+    ]
--- webproject/coures/views.py
+++ webproject/coures/views.py
@@ -1,11 +1,15 @@
 
 # Create your views here.
 from django.shortcuts import render
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView , DeleteView
 from django.urls import reverse_lazy
 from .models import *
 
 # Create your views here.
+
+def course_list(request):
+    courses = course.objects.all()  # ดึงรายการรายวิชาทั้งหมด
+    return render(request, 'course_list.html', {'courses': courses})
 
 def course_search(request):
     search = request.GET.get('id', '')
@@ -19,3 +23,7 @@ class CourseUpdateView(UpdateView):
     template_name = 'course_update.html'
     success_url = reverse_lazy('course_search')
 
+class CourseDeleteView(DeleteView):
+    model = course
+    template_name = 'course_delete.html'
+    success_url = reverse_lazy('course_search')
