14987d52769abec6498b759936a1d190c900fb28
Kittiphod-ka <kittiphod.ka.65@ubu.ac.th>
2024-12-03 09:43:53 +0700

    Update urls.py

--- webproject/coures/urls.py
+++ webproject/coures/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('course_search/', course_search, name='course_search'),
     path('update/<str:pk>/', CourseUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', CourseDeleteView.as_view(), name='delete'),
-    path('courses/', course_list, name='course_list'),
+    path('courses/', course_list.as_view(), name='course_list'),
 
     ]
